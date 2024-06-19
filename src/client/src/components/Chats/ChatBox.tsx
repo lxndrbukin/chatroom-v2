f89573b4d327c0435e4fd7839ac9813e79d741dd
lxndrbukin <lxndrbukin@gmail.com>
2024-06-19 07:21:28 +0100

    Add initial functionality for sending the message from the server back to the client and pushing into the ChatBox

--- src/client/src/components/Chats/ChatBox.tsx
+++ src/client/src/components/Chats/ChatBox.tsx
@@ -1,10 +1,24 @@
 import './assets/styles.scss';
+import { useEffect, useState } from 'react';
+import { socket } from '../../socket';
 import ChatForm from './ChatForm';
 
 export default function ChatBox(): JSX.Element {
+  const [messages, setMessages] = useState<Array<{ message: string }>>([]);
+
+  useEffect(() => {
+    socket.on('message-client', (data: any) => {
+      setMessages((prevState) => [...prevState, JSON.parse(data)]);
+    });
+  }, []);
+
+  const renderedMessages = messages.map((message) => {
+    return <div>{message.message}</div>;
+  });
+
   return (
     <div className="chat-box">
-      <div className="chat-messages"></div>
+      <div className="chat-messages">{renderedMessages}</div>
       <ChatForm />
     </div>
   );
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -4,7 +4,7 @@ export const io = (socketIO: Socket) => {
   socketIO.on('connection', (socket) => {
     console.log(socket.id, 'just connected');
     socket.on('message', (data: any) => {
-      console.log(JSON.parse(data));
+      socketIO.emit('message-client', data);
     });
   });
 };
