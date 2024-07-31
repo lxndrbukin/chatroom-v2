46cf68d287775ec29a192508b93beb37af5cc7d7
lxndrbukin <lxndrbukin@gmail.com>
2024-07-31 07:38:53 +0100

    Creating Header

--- src/client/src/components/App.tsx
+++ src/client/src/components/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header/Header';
 
 export default function App(): JSX.Element {
   return (
     <main className="app">
+      <Header />
       <Outlet />
     </main>
   );
