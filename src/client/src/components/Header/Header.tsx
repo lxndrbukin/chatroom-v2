ef5ce0c159660b8c3b75f033cc9ff897a8510b55
lxndrbukin <lxndrbukin@gmail.com>
2024-08-07 07:36:17 +0100

    Refactor Header. Create separate a separate HeaderSearch component and a reusable HeaderUserSection component for future separate User components on the Header

--- src/client/src/components/Header/Header.tsx
+++ src/client/src/components/Header/Header.tsx
@@ -1,27 +1,21 @@
 import './assets/styles.scss';
 import { Link } from 'react-router-dom';
+import HeaderSearch from './HeaderSearch';
+import HeaderUserSection from './HeaderUserSection';
 import { PiUserCircle } from 'react-icons/pi';
-import { IoSearchOutline } from 'react-icons/io5';
+import { BsChatRightFill } from 'react-icons/bs';
 
 export default function Header(): JSX.Element {
   return (
     <header className="header">
       <div className="header-container">
         <Link to="/" className="header-logo">
+          <BsChatRightFill />
           ChatRoom
         </Link>
-        <form className="header-search">
-          <input name="search" placeholder="Search..." />
-          <button>
-            <IoSearchOutline />
-          </button>
-        </form>
+        <HeaderSearch />
         <div className="header-user">
-          <div className="header-user-profile">
-            <div className="header-user-icon">
-              <PiUserCircle />
-            </div>
-          </div>
+          <HeaderUserSection icon={PiUserCircle} />
         </div>
       </div>
     </header>
