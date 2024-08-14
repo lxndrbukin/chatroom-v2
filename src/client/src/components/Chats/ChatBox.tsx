e937bd5f6f633857a8cd5e92f22d858d8022b2aa
lxndrbukin <lxndrbukin@gmail.com>
2024-08-14 07:41:17 +0100

    Refactoring the Chat ChatBox and ChatMembers components and working on styling

--- src/client/src/components/Chats/ChatBox.tsx
+++ src/client/src/components/Chats/ChatBox.tsx
@@ -1,6 +1,4 @@
-import './assets/styles.scss';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import { socket } from '../../socket';
 import { MessageType, MessageProps } from '../../socket/types';
 import { ChatMessageProps } from './types';
@@ -9,22 +7,10 @@ import ChatMessage from './ChatMessage';
 
 export default function ChatBox(): JSX.Element {
   const [messages, setMessages] = useState<Array<ChatMessageProps>>([]);
-  const [totalOnline, setTotalOnline] = useState(0);
-  const { roomId } = useParams();
 
   useEffect(() => {
-    socket.on(MessageType.TotalOnline, ({ totalOnline }: MessageProps) => {
-      if (totalOnline) setTotalOnline(totalOnline);
-    });
-
-    socket.emit(MessageType.RoomConnection, { roomId, msg: 'user connected' });
-
     socket.on(MessageType.ChatMessage, ({ msg }: MessageProps) => {
       if (msg) setMessages((prevState) => [...prevState, { msg }]);
-    });
-
-    window.addEventListener('beforeunload', () => {
-      socket.emit(MessageType.RoomDisconnection, { roomId });
     });
   }, []);
 
@@ -34,7 +20,6 @@ export default function ChatBox(): JSX.Element {
 
   return (
     <div className="chat-box">
-      <div>Total Online: {totalOnline}</div>
       <div className="chat-messages">{renderedMessages}</div>
       <ChatForm />
     </div>
