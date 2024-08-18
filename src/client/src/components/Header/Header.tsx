e3d9b16854b5679f631dd8fbe57645da911f1778
lxndrbukin <lxndrbukin@gmail.com>
2024-08-18 17:01:47 +0100

    Working on user session

--- src/client/src/components/Header/Header.tsx
+++ src/client/src/components/Header/Header.tsx
@@ -1,11 +1,23 @@
 import './assets/styles.scss';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 import HeaderSearch from './HeaderSearch';
 import HeaderUserSection from './HeaderUserSection';
 import { FaUserCircle } from 'react-icons/fa';
 import { BsChatRightFill } from 'react-icons/bs';
 
 export default function Header(): JSX.Element {
+  const { isLoggedIn, data } = useSelector((state: RootState) => state.session);
+
+  const loginBtn = <button className="header-auth-btn">Login</button>;
+
+  const userSection = isLoggedIn ? (
+    <HeaderUserSection icon={FaUserCircle} />
+  ) : (
+    loginBtn
+  );
+
   return (
     <header className="header">
       <div className="header-container">
@@ -14,9 +26,7 @@ export default function Header(): JSX.Element {
           ChatRoom
         </Link>
         <HeaderSearch />
-        <div className="header-user">
-          <HeaderUserSection icon={FaUserCircle} />
-        </div>
+        <div className="header-user">{userSection}</div>
       </div>
     </header>
   );
