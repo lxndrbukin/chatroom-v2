28f4ae29bcbc98be4d80c554e7cb7c5bc6a7229e
lxndrbukin <lxndrbukin@gmail.com>
2024-09-25 07:28:50 +0100

    Minor changes to the Header search bar

--- src/client/src/components/Header/HeaderSearch.tsx
+++ src/client/src/components/Header/HeaderSearch.tsx
@@ -1,9 +1,21 @@
+import { FocusEvent, useState } from 'react';
 import { IoSearchOutline } from 'react-icons/io5';
 
 export default function HeaderSearch(): JSX.Element {
+  const [select, setSelect] = useState<boolean>(false);
+
+  const handleSelect = (e: FocusEvent<HTMLInputElement>) => {
+    setSelect(!select);
+  };
+
   return (
-    <form className="header-search">
-      <input name="search" placeholder="Search..." />
+    <form className={`header-search ${select ? 'selected' : ''}`}>
+      <input
+        onSelect={handleSelect}
+        onBlur={handleSelect}
+        name="search"
+        placeholder="Search..."
+      />
       <button>
         <IoSearchOutline />
       </button>
